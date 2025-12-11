81f356bebba44b8ffabca9450303afd3ad931028
MyCloudG <126359876+MyCloudG@users.noreply.github.com>
2025-12-11 15:26:32 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,23 +1,3 @@
-/*
-    Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    https://github.com/alexemanuelol/rustplusplus
-
-*/
-
 module.exports = {
     general: {
         language: process.env.RPP_LANGUAGE || 'en',
@@ -27,8 +7,11 @@ module.exports = {
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
-        clientId: process.env.RPP_DISCORD_CLIENT_ID || '',
-        token: process.env.RPP_DISCORD_TOKEN || '',
-        needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true, /* If true, only admins can delete (server, switch..), manage credentials and reset a channel */
+        clientId: process.env.RPP_DISCORD_CLIENT_ID || '1448661997955584183',
+
+        // ❗ token už nesmie byť uložený v kóde
+        token: process.env.RPP_DISCORD_TOKEN,
+
+        needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true,
     }
 };
